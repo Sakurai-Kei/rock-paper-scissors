d0b63135a4cf92dfc2300aa580473f6055d81ed2
R_Ashfell <luqmanulraiserhakim@gmail.com>
2021-10-03 20:40:51 +0800

    Done with gameStart function. Moving on with game function.

--- myScript.js
+++ myScript.js
@@ -1,3 +1,9 @@
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+let result;
+
+console.log(gameStart(playerSelection,computerSelection))
+
 function computerPlay() {
     let computerSelection;
     let computerRNG = Math.floor(Math.random()*(4-1)+1);
@@ -16,6 +22,57 @@ function computerPlay() {
     return computerSelection;
 }
 
-function gameStart() {
+function gameStart(playerSelection,computerSelection) {
+    let playerSelectionCap = playerSelection.toUpperCase();
+    let computerSelectionCap = computerSelection.toUpperCase();
 
+    if(playerSelectionCap === "ROCK"){
+        if(computerSelectionCap === "ROCK"){
+            result = `A Draw. You choose ${playerSelectionCap}
+                            and Computer choose ${computerSelectionCap}`;              
+        }
+        else if(computerSelectionCap === "PAPER"){
+            result = `You Lose. You choose ${playerSelectionCap}
+                             and Computer choose ${computerSelectionCap}`;
+        }
+        else{
+            result = `You Win. You choose ${playerSelectionCap}
+                            and Computer choose ${computerSelectionCap}`;
+        }
+    }
+    else if(playerSelectionCap === "PAPER"){
+        if(computerSelectionCap === "ROCK"){
+            result = `You Win. You choose ${playerSelectionCap}
+                            and Computer choose ${computerSelectionCap}`;
+        }
+        else if(computerSelectionCap === "Paper"){
+            result = `A Draw. You choose ${playerSelectionCap}
+                             and Computer choose ${computerSelectionCap}`;
+        }
+        else{
+            result = `You Lose. You choose ${playerSelectionCap}
+                            and Computer choose ${computerSelectionCap}`;
+        }
+        return result;
+    }
+    else if(playerSelectionCap === "SCISSORS"){
+        if(computerSelectionCap === "ROCK"){
+            result = `You Lose. You choose ${playerSelectionCap}
+                            and Computer choose ${computerSelectionCap}`;
+        }
+        else if(computerSelectionCap === "Paper"){
+            result = `You Win. You choose ${playerSelectionCap}
+                             and Computer choose ${computerSelectionCap}`;
+        }
+        else{
+            result = `A Draw. You choose ${playerSelectionCap}
+                            and Computer choose ${computerSelectionCap}`;
+        }
+        return result;
+    }
+    else{
+        result = `Please enter the correct input. You entered ${playerSelection}`;
+    }
+    return result;
 }
+
